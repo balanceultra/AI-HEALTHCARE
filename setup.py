6c1ff126d01a92b7f04d96c5aab457dd99e924e8
Aryan Sharma <aruparashar@gmail.com>
2025-03-25 07:16:47 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Ai Healthcare Project',
+    version= '0.0.0',
+    author= 'Aryan Sharma',
+    author_email= 'aruparashar@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
